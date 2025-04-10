2ba6b3cb92bc51b11afc6784baa972c76e410cf0
Mutaqin Yusuf <mutaqinyusuf73@gmail.com>
2025-04-10 19:28:50 +0700

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teramedik Soal A - PHP</title>
+    <script src="/assets/tailwind.js"></script>
+</head>
+<body>
+    <div class="h-screen w-screen flex items-center justify-center">
+        <div class="h-[50%] text-center flex flex-col gap-5 border border-gray-300 p-10 rounded bg-white">
+            <h1 class="text-3xl">Screening Teramedik Soal A (PHP)</h1>
+            <div class="flex justify-center gap-1 mt-10">
+                <a href="/soal-1" class="cursor-pointer bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
+                    Soal 1
+                </a>
+                <a href="/soal-2" class="cursor-pointer bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
+                    Soal 2
+                </a>
+            </div>
+            <p>Programmed by <a class="font-bold text-blue-500 underline" target="_blank" href="https://www.linkedin.com/in/mutaqin-yusuf/">Mutaqin Yusuf</a></p>
+        </div>
+    </div>
+</body>
+</html>
